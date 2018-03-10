43ac5429df8f6dbcdc4768f97592d24df791ac9f
Nishant Rawat <nishantrawat22@gmail.com>
2018-03-10 15:24:42 -0700

    Using annotations and session.get

--- src/main/java/net/codejava/hibernate/dto/UserDetails.java
+++ src/main/java/net/codejava/hibernate/dto/UserDetails.java
@@ -1,7 +1,12 @@
 package net.codejava.hibernate.dto;
 
+import java.util.Date;
+
 import javax.persistence.Entity; //Instead of using hibernate api we'll use persistence api
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class UserDetails {
@@ -19,5 +24,29 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	private String userName;
+	
+	private String address;
+	@Temporal ( TemporalType.DATE)
+	private Date joinedDate;
+	@Lob
+	private String description;
 }
--- src/main/java/org/nishant/hibernate/HibernateTest.java
+++ src/main/java/org/nishant/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.nishant.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,16 +9,33 @@ import org.hibernate.cfg.Configuration;
 import net.codejava.hibernate.dto.UserDetails;
 
 public class HibernateTest {
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
+		
+		//Normally we'd use DAO to create a data layer and the data layer will use hibernate to save this.
 		UserDetails user = new UserDetails();
 		user.setUserId(1);
 		user.setUserName("firstUser");
-		//Normally we'd use DAO to create a data layer and the data layer will use hibernate to save this.
+		user.setAddress("First User's address");
+		user.setJoinedDate(new Date());
+		user.setDescription("First user's description");
+		
 	
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit(); //db commit
+		session.close();
+		
+		user = null;
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		/**We ask for the class equivalent of the table that we want. UserDetails represent the table UserDetails
+		 * 1 is the value of the primary key. We don't have to specify what column is the primary key,
+		 * since we have already annotated userId as ID, and thus hibernate knows which column to lookup
+		 */
+		user = session.get(UserDetails.class, 1); 
+		System.out.println(user.getUserName());
+		
 	}
 }
